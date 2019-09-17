831a1d03b1a1b949d98f56b70d2fca6078345cbb
jahangir <jaha@ereshov.ru>
2019-09-18 00:28:19 +0300

    add ivy and projectile

---
+++ kbd/projectile.el
@@ -0,0 +1,6 @@
+;; (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+;; (define-key projectile-mode-map (kbd "C-p") 'projectile-command-map)
+
+;; projectile commandse
+(define-key global-map (kbd "M-f") 'projectile-find-file)
+sp(define-key global-map (kbd "M-d") 'projectile-find-dir)
