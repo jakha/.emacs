4712731d4bf198a73e64469f6fc87c3f6164a83e
jahangir <jaha@ereshov.ru>
2019-09-17 23:40:55 +0300

    add doom theme

---
+++ kbd/base.el
@@ -0,0 +1,27 @@
+;; buffer split and close
+(define-key global-map (kbd "M-1") 'delete-other-windows)
+(define-key global-map (kbd "M-2") 'split-window-right)
+(define-key global-map (kbd "M-3") 'split-window-below)
+(define-key global-map (kbd "M-0") 'delete-window)
+
+;; save
+(define-key global-map (kbd "M-s") 'save-buffer)
+(define-key global-map (kbd "C-k") 'kill-buffer)
+
+;; cut and paste to alt kbd
+(define-key global-map (kbd "M-q") 'kill-region)
+(define-key global-map (kbd "M-e") 'yank)
+(define-key global-map (kbd "C-a") 'mark-whole-buffer)
+
+;; switch buffer, file and project ido mode
+(define-key global-map (kbd "M-b") 'ido-switch-buffer)
+
+;; close comand line
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; other window
+(define-key global-map (kbd "M-p") 'other-window)
+(define-key global-map (kbd "M-o") 'back-window)
+(defun back-window ()
+  (interactive)
+  (other-window -1))
--- themes/common.el
+++ themes/common.el
@@ -2,3 +2,8 @@
 (tool-bar-mode -1)
 ;;(scroll-bar-mode -1)
 
+;; auto refresh buffers
+(global-auto-revert-mode 1)
+
+;; cursor type
+(setq-default cursor-type 'bar)
