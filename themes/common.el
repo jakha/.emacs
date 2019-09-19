a4e41d3277a9e34845864129c884eb0355884230
jahangir <jaha@ereshov.ru>
2019-09-19 23:44:38 +0300

    add logo to dashboard

--- themes/common.el
+++ themes/common.el
@@ -16,3 +16,21 @@
   :ensure t
   :config
   (dashboard-setup-startup-hook))
+
+;; Set the title
+(setq dashboard-banner-logo-title "Welcome to Emacs Dashboard")
+;; Set the banner
+(setq dashboard-startup-banner (concat user-emacs-directory "themes/imgs/logo1.png"))
+;; Value can be
+;; 'official which displays the official emacs logo
+;; 'logo which displays an alternative emacs logo
+;; 1, 2 or 3 which displays one of the text banners
+;; "path/to/your/image.png" which displays whatever image you would prefer
+
+;; Content is not centered by default. To center, set
+(setq dashboard-center-content t)
+
+;; To disable shortcut "jump" indicators for each section, set
+(setq dashboard-show-shortcuts nil)
+(setq dashboard-set-heading-icons t)
+(setq dashboard-set-file-icons t)
--- themes/font.el
+++ themes/font.el
@@ -1,34 +1,34 @@
-(use-package cnfonts
-  :disabled t
-  :straight t
-  :init
-  (gsetq cnfonts-directory (no-littering-expand-etc-file-name "cnfonts"))
-  :hook ((after-init . cnfonts-enable))
-  :config
-  (defun nasy/set-symbol-fonts (fontsize-list)
-    "Set symbol fonts with FONTSIZE-LIST."
-    (let* ((fontname "Fira Code Symbol")
-           (fontsize (nth 0 fontsize-list))
-           (fontspec (font-spec :name fontname
-                                :size fontsize
-                                :weight 'normal
-                                :slant 'normal)))
-      (if (cnfonts--fontspec-valid-p fontspec)
-          (set-fontset-font t '(#Xe100 . #Xe16f) fontspec)
-        (message "Font %S not exists！" fontname))))
-  (defun nasy/set-symbol-extra-fonts (fontsize-list)
-    "Set extra symbol fonts with FONTSIZE-LIST."
-    (let* ((fontname "Arial")
-           (fontsize (nth 0 fontsize-list))
-           (fontspec (font-spec :name fontname
-                                :size fontsize
-                                :weight 'normal
-                                :slant 'normal)))
-      (if (cnfonts--fontspec-valid-p fontspec)
-          (set-fontset-font t '(#X1d400 . #X1d744) fontspec)
-        (message "Font %S not exists！" fontname))))
-  (add-hook #'cnfonts-set-font-finish-hook #'nasy/set-symbol-fonts)
-  (add-hook #'cnfonts-set-font-finish-hook #'nasy/set-symbol-extra-fonts))
+;; (use-package cnfonts
+;;   :disabled t
+;;   :straight t
+;;   :init
+;;   (gsetq cnfonts-directory (no-littering-expand-etc-file-name "cnfonts"))
+;;   :hook ((after-init . cnfonts-enable))
+;;   :config
+;;   (defun nasy/set-symbol-fonts (fontsize-list)
+;;     "Set symbol fonts with FONTSIZE-LIST."
+;;     (let* ((fontname "Fira Code Symbol")
+;;            (fontsize (nth 0 fontsize-list))
+;;            (fontspec (font-spec :name fontname
+;;                                 :size fontsize
+;;                                 :weight 'normal
+;;                                 :slant 'normal)))
+;;       (if (cnfonts--fontspec-valid-p fontspec)
+;;           (set-fontset-font t '(#Xe100 . #Xe16f) fontspec)
+;;         (message "Font %S not exists！" fontname))))
+;;   (defun nasy/set-symbol-extra-fonts (fontsize-list)
+;;     "Set extra symbol fonts with FONTSIZE-LIST."
+;;     (let* ((fontname "Arial")
+;;            (fontsize (nth 0 fontsize-list))
+;;            (fontspec (font-spec :name fontname
+;;                                 :size fontsize
+;;                                 :weight 'normal
+;;                                 :slant 'normal)))
+;;       (if (cnfonts--fontspec-valid-p fontspec)
+;;           (set-fontset-font t '(#X1d400 . #X1d744) fontspec)
+;;         (message "Font %S not exists！" fontname))))
+;;   (add-hook #'cnfonts-set-font-finish-hook #'nasy/set-symbol-fonts)
+;;   (add-hook #'cnfonts-set-font-finish-hook #'nasy/set-symbol-extra-fonts))
 
 ;; (set-face-attribute
 ;;  'default nil
