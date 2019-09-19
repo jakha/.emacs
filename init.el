28d0f30deeb47f0c5ac4c3e464a891b0ecce78c5
jahangir <jaha@ereshov.ru>
2019-09-19 11:09:42 +0300

    init projects

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
@@ -9,13 +10,11 @@
 
 (add-to-list 'load-path (concat user-emacs-directory "utils/"))
 (require 'load-directory)		
+(load "add-elpa-melpa.el")
 
 (load-directory (concat user-emacs-directory "themes/"))
 (load-directory (concat user-emacs-directory "kbd/"))
 
-(require 'package)
-(add-to-list 'package-archives
-             '("MELPA Stable" . "http://stable.melpa.org/packages/") t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -23,7 +22,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (treemacs-projectile use-package projectile counsel ivy doom-themes))))
+    (cnfonts treemacs-projectile use-package projectile counsel ivy doom-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -31,11 +30,17 @@
  ;; If there is more than one, they won't work right.
  )
 
+;;ivy
 (ivy-mode 1)
+
+;;projectile
 (require 'projectile)
 (setq projectile-completion-system 'ivy)
+(setq projectile-enable-caching t)
+(setq projectile-indexing-method 'alien)
 (projectile-mode +1)
 
+;;treemacs
 (use-package treemacs
   :ensure t
   :defer t
@@ -45,11 +50,19 @@
   :config
   (progn
     (setq treemacs-width 25
-	  treemacs-toggle-fixed-width t))
+	  treemacs-toggle-fixed-width 1))
   :bind
   (:map global-map
-	("<f12>" . treemacs)))
+	("<f12>" . treemacs)
+	("C-t" . treemacs-add-project-to-workspace)))
 
 (use-package treemacs-projectile
   :after treemacs projectile
   :ensure t)
+
+(use-package uniquify
+  :init  ;; nicer naming of buffers for files with identical names
+  (setq uniquify-buffer-name-style   'reverse
+        uniquify-separator           " • "
+        uniquify-after-kill-buffer-p t
+        uniquify-ignore-buffers-re   "^\\*"))
